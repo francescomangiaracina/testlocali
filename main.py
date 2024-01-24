dbecfa4151722749025d2d40065191f418192a01
francescomangiaracina <fmangiaracina@cgnal.com>
2024-01-24 14:48:49 +0100

    Aggiornamenti parte 9

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 
 
 @app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/items/{item_id}")
 async def read_item(item_id: str, q: str | None = None):
     if q:
         return {"item_id": item_id, "q": q}
