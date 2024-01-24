285eef07aaf74560746d04f0c99d0286d509a49c
francescomangiaracina <fmangiaracina@cgnal.com>
2024-01-24 14:42:52 +0100

    Aggiornamenti parte 7

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 @app.get("/")
 async def root():
